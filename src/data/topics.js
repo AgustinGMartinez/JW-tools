09f890b7bc4284191a9d3a885d2997c24e9a2c8f
Agustin G Martinez <zhuclam@gmail.com>
2019-10-10 16:17:19 -0300

    fixed jere 29 11, added 2 others

--- src/data/topics.js
+++ src/data/topics.js
@@ -8,8 +8,8 @@ export default [
 			'amos-9-6',
 			'nume-19-16',
 			'jere-25-12',
-			'2tim-3-16',
-		],
+			'2tim-3-16'
+		]
 	},
 	{
 		displayName: 'Religión',
@@ -24,8 +24,8 @@ export default [
 			'reve-1-1',
 			'juan-11-51:52',
 			'1jua-5-3',
-			'hech-8-30:31',
-		],
+			'hech-8-30:31'
+		]
 	},
 	{
 		displayName: 'Un solo Dios',
@@ -46,8 +46,8 @@ export default [
 			'mate-3-16:17', // dios aprobo a jesus, el no podia hacerselo a si mismo
 			'juan-6-27', // el padre, dios, aprobo a jesus
 			'reve-1-1', // dios le dio una revelacion a jesus
-			'gala-1-1', // jesuscristo, y dios el padre que lo levanto de entre los muertos
-		],
+			'gala-1-1' // jesuscristo, y dios el padre que lo levanto de entre los muertos
+		]
 	},
 	{
 		displayName: 'Mundo de satanás',
@@ -56,10 +56,10 @@ export default [
 			'2tim-3-1:5',
 			'jere-10-23',
 			'reve-12-12',
-			'jere-9-11',
+			'jere-29-11',
 			'mate-6-9:10',
-			'dani-2-44',
-		],
+			'dani-2-44'
+		]
 	},
 	{
 		displayName: 'Muerte',
@@ -72,8 +72,8 @@ export default [
 			'1cor-15-26',
 			'hech-24-15',
 			'juan-5-28:29',
-			'1tes-4-16',
-		],
+			'1tes-4-16'
+		]
 	},
 	{
 		displayName: 'Dios se preocupa por nosotros',
@@ -86,23 +86,22 @@ export default [
 			'mate-10-29:31',
 			'salm-139-1:4',
 			'fili-4-6:7',
-			'prov-17-22',
-		],
+			'prov-17-22'
+		]
 	},
 	{
 		displayName: 'Paraíso',
 		verses: [
 			'gene-1-28',
 			'mate-5-5',
-			'gene-1-28',
 			'isai-55-11',
 			'salm-37-10:11',
 			'salm-37-29',
 			'2ped-3-13',
 			'reve-5-10',
 			'reve-11-18',
-			'reve-21-3:4',
-		],
+			'reve-21-3:4'
+		]
 	},
 
 	{
@@ -118,32 +117,32 @@ export default [
 			'dani-12-4',
 			'1tes-5-2',
 			'reve-14-15',
-			'reve-16-13:16',
-		],
+			'reve-16-13:16'
+		]
 	},
 	{
 		displayName: 'Nombre de Dios',
-		verses: ['salm-83-18', 'jere-23-27'],
+		verses: ['salm-83-18', 'jere-23-27']
 	},
 	{
 		displayName: 'Reunirse',
-		verses: ['hebr-10-24', 'efes-4-11:16'],
+		verses: ['1cor-12-26:27', 'hebr-10-24', 'efes-4-11:16']
 	},
 	{
 		displayName: 'Aborto',
-		verses: ['exod-21-22:23'],
+		verses: ['exod-21-22:23']
 	},
 	{
 		displayName: 'Imágenes',
-		verses: ['exod-20-4', 'isai-42-8'],
+		verses: ['exod-20-4', 'isai-42-8']
 	},
 	{
 		displayName: 'Sangre',
-		verses: ['gene-9-4', 'levi-17-10:11', 'hech-15-20', 'hech-15-28:29'],
+		verses: ['gene-9-4', 'levi-17-10:11', 'hech-15-20', 'hech-15-28:29']
 	},
 	{
 		displayName: 'Festividades',
-		verses: ['fili-1-10'],
+		verses: ['fili-1-10']
 	},
 	{
 		displayName: 'Política',
@@ -153,8 +152,8 @@ export default [
 			'efes-6-12',
 			'juan-6-15',
 			'juan-17-16',
-			'juan-18-36',
-		],
+			'juan-18-36'
+		]
 	},
 	{
 		displayName: 'Enfermedades',
@@ -163,8 +162,8 @@ export default [
 			'isai-35-5:6',
 			'salm-41-3',
 			'fili-4-6:7',
-			'prov-17-22',
-		],
+			'prov-17-22'
+		]
 	},
 	{
 		displayName: 'Debemos hacer cambios',
@@ -175,23 +174,23 @@ export default [
 			'1tes-5-2',
 			'mate-24-37:39',
 			'juan-3-36',
-			'sant-2-24:26',
-		],
+			'sant-2-24:26'
+		]
 	},
 	{
 		displayName: 'Infierno',
-		verses: ['juan-3-16', 'roma-6-7', '1jua-4-8', 'reve-20-14'],
+		verses: ['juan-3-16', 'roma-6-7', '1jua-4-8', 'reve-20-14']
 	},
 	{
 		displayName: 'Expulsión',
-		verses: ['2tes-3-14:15', '1cor-5-11', '1cor-5-5', '1tim-1-20'],
+		verses: ['2tes-3-14:15', '1cor-5-11', '1cor-5-5', '1tim-1-20']
 	},
 	{
 		displayName: 'Salvación',
-		verses: ['mate-19-17', 'juan-3-16', 'juan-17-3'],
+		verses: ['mate-19-17', 'juan-3-16', 'juan-17-3']
 	},
 	{
 		displayName: 'Incesto',
-		verses: ['levi-18-9'],
-	},
+		verses: ['levi-18-9']
+	}
 ]
